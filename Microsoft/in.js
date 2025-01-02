3df29b6216ff2ba43de09dfd962d4625c2d464d0
prafulstha98 <prafulstha98@gmail.com>
2025-01-02 20:00:20 +0545

    updated

---
+++ Microsoft/in.js
@@ -0,0 +1,24 @@
+// const headTag = document.querySelector("h1");
+
+// const changeHead = () => {
+//   headTag.style.color = "red";
+//   headTag.style.fontSize = "50px";
+// };
+
+const headTag = document.querySelector("h1");
+
+const changeHead = () => {
+  headTag.style.color = "blue";
+  headTag.style.fontSize = "100px";
+};
+
+const laptop = ["hp", "dell", "asus", "lenovo", "razor"];
+
+console.log(laptop[0]);
+
+let a = 51;
+let b = 66;
+
+b = 515;
+console.log("The value of a is ", a);
+console.log("The value of b is ", b);
