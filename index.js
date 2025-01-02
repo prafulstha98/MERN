35fc5fec1acc32b02410e903ca2ecd9336b927f7
kali <kali@kali>
2025-01-02 21:27:09 +0545

    :sparkles: added leapyear program

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ console.log(personName + "kjhjhjk");
 console.log(personName.includes("rl"));
 
 console.log(m.repeat(2));
+
+
+//leap year: write a program to check if a given year is a leap year
+let year = 2100;
+if(year % 400 === 0){
+    console.log(year,"is a leap year");
+}else if(year % 100 === 0){
+    console.log(year,"is not a leap year");
+}else if(year % 4 === 0){
+    console.log(year, "is a leap year");
+}else{
+    console.log(year, "is not a leap year");
+}
