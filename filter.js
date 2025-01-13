09546fe54f7d4caebe48a4f6ef9a773a58d39382
prafulstha98 <prafulstha98@gmail.com>
2025-01-13 08:10:08 +0545

    changes

--- filter.js
+++
@@ -1,7 +0,0 @@
-const numbers = [11, 22, 33, 44, 55, 66, 77, 88, 99];
-
-const n = numbers.reduce((a, b) => {
-  //find maximum and minimum value using array reduce method
-});
-
-console.log(n);
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Heading 1</h1>
+      <p>This is a paragraph tag</p>
+    </div>
+  );
+};
+
+export default App;
